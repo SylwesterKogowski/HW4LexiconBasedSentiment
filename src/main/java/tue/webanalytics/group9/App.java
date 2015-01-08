8ff1dc23f1b613a5abd3c539113c7aba46c197ba
Sylwester Kogowski <sylwekwazuuup@gmail.com>
2015-01-08 22:46:17 +0100

    etc

--- src/main/java/tue/webanalytics/group9/App.java
+++ src/main/java/tue/webanalytics/group9/App.java
@@ -1,11 +1,15 @@
 package tue.webanalytics.group9;
 
+import net.didion.jwnl.data.Exc;
 import org.deeplearning4j.text.inputsanitation.InputHomogenization;
 import org.deeplearning4j.text.sentenceiterator.FileSentenceIterator;
 import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
 import org.deeplearning4j.text.sentenceiterator.SentencePreProcessor;
+import org.omg.CORBA_2_3.portable.*;
 
 import java.io.*;
+import java.io.InputStream;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Dictionary;
 import java.util.Enumeration;
@@ -21,30 +25,31 @@ public class App
 
     WordCollection wordCollection = new WordCollection();
 
-    public static int main( String[] args )
+    public static void main( String[] args )
     {
-        if(args.length != 2)
+        if(args.length != 1)
         {
             System.out.println("Usage: "+args[0]+" <input file name>");
             System.out.println("in the input file:");
             System.out.println("  each line is a phrase to check sentiment");
-            return 1;
+            return;
         }
 
-        File input = new File(args[1]);
-        if(!input.exists() || !input.canRead())
+
+        File input = new File(args[0]);
+        /*if(!input.exists() || !input.canRead())
         {
-            System.out.println("File "+args[1]+" doesn't exist");
-            return 2;
-        }
+            System.out.println("File "+args[0]+" doesn't exist");
+            return ;
+        }*/
         App app = new App();
         try {
             app.start(input);
         } catch (Exception e) {
             e.printStackTrace();
-            return 1;
+            return ;
         }
-        return 0;
+        return ;
     }
 
      void start(File inputFile) throws Exception {
@@ -53,18 +58,32 @@ public class App
          processInput(inputFile);
      }
 
-    private void processInput(File inputFile) {
+    private void processInput(File inputFile) throws Exception {
+        if(!inputFile.exists())
+        {
+            throw new Exception("Input file "+inputFile.getName()+" doesn't exist");
+        }
+        if(!inputFile.canRead())
+        {
+            throw new Exception("Input file "+inputFile.getName()+" cannot be read");
+        }
+
         //TODO:finish this
     }
 
 
     void loadSentiWordNetVectors() throws Exception {
-        File sentiwordnet = new File(getClass().getResource("/SentiWordNet_3.0.0_20130122.txt").toURI());
-        BufferedReader br = new BufferedReader(new FileReader(sentiwordnet));
+        InputStream is = getClass().getResourceAsStream("/SentiWordNet_3.0.0_20130122.txt");
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
         String line;
         while((line = br.readLine()) != null)
         {
+            if(line.charAt(0) == '#')
+                continue;
             String[] s = line.split("\t");
+
+            if(s.length < 5)
+                continue;
             int id = Integer.parseInt(s[1]);
             float positive = Float.parseFloat(s[2]);
             float negative = Float.parseFloat(s[3]);

ca660e750ea838c764f11370676ffa47b9a00471
Sylwester Kogowski <sylwekwazuuup@gmail.com>
2015-01-08 17:17:47 +0100

    Initial commit

---
+++ src/main/java/tue/webanalytics/group9/App.java
@@ -0,0 +1,13 @@
+package tue.webanalytics.group9;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
